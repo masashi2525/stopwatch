ab5d44e7a55a72814d310d5f99050e8c59ad3649
masashi2525 <noerunoe8@gmail.com>
2020-04-29 11:59:29 +0900

    ストップウォッチ作成

---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let start;
+let timer_id;
+
+document.getElementById('start_stop').addEventListener('click', function(){
+  if(this.innerHTML ==='START') {
+    start = new Date();
+    timer_id = setInterval(goTimer, 10);
+    this.innerHTML = 'STOP';
+    this.classList.remove('btn-primary');
+    this.classList.add('btn-danger');
+  } else {
+    clearInterval(timer_id);
+    this.innerHTML = 'START';
+    this.classList.remove('btn-danger');
+    this.classList.add('btn-primary');
+  }
+});
+
+const addZero = function(value) {
+  if(value < 10) {
+    value = '0' + value;
+  }
+  return value;
+}
+
+const goTimer = function() {
+  const now = new Date();
+  const milli = now.getTime() - start.getTime();
+  let seconds = Math.floor(milli / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  seconds = seconds - minutes * 60;
+  minutes = minutes - hours * 60;
+  document.getElementById('timer').innerHTML = addZero(hours) + ':' + addZero(minutes) + ':' + addZero(seconds);
+};
